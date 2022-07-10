cd95f1a2ae825d31ae2c1d5eb99b8bb384787d54
Son Dinh Thai <87920408+iamironman1233@users.noreply.github.com>
2022-07-11 04:21:03 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+function clickIE4() {
+		return 2 == event.button ? !1 : void 0
+	}
+
+	function clickNS4(e) {
+		return !(document.layers || document.getElementById && !document.all) || 2 != e.which && 3 != e.which ? void 0 : !1
+	}
+	$(document).ready(function() {
+		const e = $("body"),
+			n = $(".btn");
+		n.on("click", function() {
+			n.removeClass("active"), $(this).addClass("active"), e.toggleClass("dark")
+		})
+	});
+	var message = "Haizz";
+	document.addEventListener("keydown", function(e) {
+		e.ctrlKey && e.preventDefault()
+	}), document.layers ? (document.captureEvents(Event.MOUSEDOWN), document.onmousedown = clickNS4) : document.all && !document.getElementById && (document.onmousedown = clickIE4), document.oncontextmenu = new Function("return false"), document.onkeypress = function(e) {
+		return e = e || window.event, 123 == e.keyCode ? !1 : void 0
+	}, document.onmousedown = function(e) {
+		return e = e || window.event, 123 == e.keyCode ? !1 : void 0
+	}, document.onkeydown = function(e) {
+		return e = e || window.event, 123 == e.keyCode ? !1 : void 0
+	};
